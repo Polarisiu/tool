79df89aae7083bbbda00836379f7bed931a1a424
Polaris <fdyrwaas@outlook.com>
2025-09-30 22:32:32 +0800

    Update mhgl.sh

--- mhgl.sh
+++ mhgl.sh
@@ -10,8 +10,7 @@ menu() {
     echo -e "${GREEN}1) 单色美化${RESET}"
     echo -e "${GREEN}2) 彩色美化${RESET}"
     echo -e "${GREEN}0) 退出${RESET}"
-    echo
-    read -p $'\033[32m请选择操作 (0-2): \033[0m' choice
+    read -p $'\033[32m请选择操作: \033[0m' choice
     case $choice in
         1)
             echo -e "${GREEN}正在运行单色美化脚本...${RESET}"
